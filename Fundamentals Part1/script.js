8286459ed01631a0300af364a0bb14d8080b3661
Guilherme Fraga <guilhermesouzasw@gmail.com>
2026-03-18 21:24:46 -0300

    Connected HTML to JavaScript file and added a title to the page. Learned Variables and Data Types in JavaScript.

---
+++ Fundamentals Part1/script.js
@@ -0,0 +1,16 @@
+//Variable declarations
+let country = "Brasil";
+let continent = "América do Sul";
+let population = 210;
+let isIsland = false;
+let language;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
