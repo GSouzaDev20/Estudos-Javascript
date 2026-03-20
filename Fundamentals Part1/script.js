728505d6395befcf75bf180c3285f975473e9b4b
Guilherme Fraga <119956812+GSouzaDev20@users.noreply.github.com>
2026-03-20 10:55:33 -0300

    Learning Basic operators and comparators

--- Fundamentals Part1/script.js
+++ Fundamentals Part1/script.js
@@ -2,6 +2,8 @@
 const country = "Brasil";
 const continent = "América do Sul";
 let population = 210;
+const finlandPopulation = 6;
+const AVERAGEPOPULATION = 33;
 const isIsland = false;
 let language = "Português";
 
@@ -14,3 +16,26 @@ console.log(typeof continent);
 console.log(typeof population);
 console.log(typeof isIsland);
 console.log(typeof language);
+
+//Basic Operations and comparators
+
+let halfPopulation = population / 2;
+
+console.log('Half of the country is :' + halfPopulation + ' Millions');
+
+    if (population > finlandPopulation){
+    console.log("The population of your country are higher");
+    }
+    else{
+    console.log("The population of your country are lower");
+    }
+
+    if (population > AVERAGEPOPULATION){
+        console.log("The population of your country are higher than the average population");
+    }
+    else{
+    console.log("The population of your country are lower than the average population");
+    }
+
+    let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+
